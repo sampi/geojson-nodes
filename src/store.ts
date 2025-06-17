f10e2955741ee3106fc3447014b1ad6730f894fd
Daniel Spitzer <sampi@sampi.io>
2025-06-17 22:26:14 +0200

    Persist nodes and edges in localStorage

--- src/store.ts
+++ src/store.ts
@@ -1,32 +1,44 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { addEdge, applyNodeChanges, applyEdgeChanges } from "@xyflow/react";
 
 import { initialNodes } from "./nodes";
 import { initialEdges } from "./edges";
 import { type AppState } from "./types";
 
-export const useStore = create<AppState>((set, get) => ({
-  nodes: initialNodes,
-  edges: initialEdges,
-  onNodesChange: (changes) => {
-    set({
-      nodes: applyNodeChanges(changes, get().nodes),
-    });
-  },
-  onEdgesChange: (changes) => {
-    set({
-      edges: applyEdgeChanges(changes, get().edges),
-    });
-  },
-  onConnect: (connection) => {
-    set({
-      edges: addEdge(connection, get().edges),
-    });
-  },
-  setNodes: (nodes) => {
-    set({ nodes });
-  },
-  setEdges: (edges) => {
-    set({ edges });
-  },
-}));
+export const useStore = create<AppState>()(
+  persist(
+    (set, get) => ({
+      nodes: initialNodes,
+      edges: initialEdges,
+      onNodesChange: (changes) => {
+        set({
+          nodes: applyNodeChanges(changes, get().nodes),
+        });
+      },
+      onEdgesChange: (changes) => {
+        set({
+          edges: applyEdgeChanges(changes, get().edges),
+        });
+      },
+      onConnect: (connection) => {
+        set({
+          edges: addEdge(connection, get().edges),
+        });
+      },
+      setNodes: (nodes) => {
+        set({ nodes });
+      },
+      setEdges: (edges) => {
+        set({ edges });
+      },
+    }),
+    {
+      name: "geojson-nodes-store",
+      partialize: (state) => ({
+        nodes: state.nodes,
+        edges: state.edges,
+      }),
+    },
+  ),
+);
